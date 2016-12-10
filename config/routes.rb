8e2f7c25bfe20a200076dcd158d1f37b2ddd28ba
darenang44 <darenang44@gmail.com>
2016-12-10 14:16:46 -0800

    Change root for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   # get 'welcome/index'
-  root 'welcome#index'
 
   # creates all the routes for all the different actions that we will be adding to our app
   resources :docs
 
+  # for users logined we want the root to be the list of all the docs
+  authenticated :user do
+    # root should in the index action in the docs controller we're giving it an alias as: 'authenticated_root' so we dont have 2 roots
+    root 'docs#index', as: 'authenticated_root'
+  end
+
+  # root for visitors not logged int
+  root 'welcome#index'
 
   # rake routes in the command line will give us all the routes in our application
   #   Prefix Verb   URI Pattern              Controller#Action
